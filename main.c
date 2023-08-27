27a82b0ed9737077f1d79a978325f36e7c67f4b1
Pedro Lucas <61566405+Pedro1516@users.noreply.github.com>
2023-08-27 16:30:58 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -55,11 +56,33 @@ Porta *gerar_AND(int num_conexoes, Porta *valor_entrada1, Porta *valor_entrada2,
 	return porta_logica;
 }
 
-Porta *gerar_OR(int num_conexoes, Porta *valor_entrada1, Porta *valor_entrada2, ...){}
+Porta *gerar_OR(int num_conexoes, Porta *valor_entrada1, Porta *valor_entrada2, ...)
+{
+	Porta *porta_logica = malloc(sizeof(Porta));
+	porta_logica->tipo = 2;
+	porta_logica->quant_entradas = num_conexoes;
+
+	porta_logica->entrada = (int **)malloc(sizeof(int *) * num_conexoes);
+	porta_logica->entrada[0] = &valor_entrada1->saida;
+	porta_logica->entrada[1] = &valor_entrada2->saida;
+
+	if (num_conexoes > 2)
+	{
+		va_list entradas;
+			va_start(entradas, valor_entrada2);
+
+		for (int i = 2; i < num_conexoes; i++)
+		{
+			Porta *atual = va_arg(entradas, Porta *);
+			porta_logica->entrada[i] = &(atual->saida);
+		}
+	}
+	return porta_logica;
+}
 
 void update(Porta **portas)
 {
-	for (size_t i = 0; i < 3; i++)
+	for (size_t i = 0; i < 4; i++) //DEPOIS PRECISO TROCAR ESSE 4 POR UMA VARIÁVEL DE TAMANHO DA REDE DE PORTAS 
 	{
 		switch (portas[i]->tipo)
 		{
@@ -85,8 +108,9 @@ void update(Porta **portas)
 
 			for (size_t j = 0; j < portas[i]->quant_entradas; j++)
 			{
+				
 				//Se pelo menos uma entrada for 1,  a saida sera 1
-				if ((*portas[i]->entrada[j]) == 0)
+				if ((*portas[i]->entrada[j]) == 1)
 				{
 					portas[i]->saida = 1;
 					break;
@@ -98,6 +122,12 @@ void update(Porta **portas)
 	}
 }
 
+
+void user_cria_porta(){
+	
+}
+
+
 int main()
 {
 	Porta sinalA;
@@ -105,6 +135,7 @@ int main()
 
 	Porta **rede_portas = (Porta **)malloc(sizeof(Porta *) * 10);
 
+//Area de criacao de portas
 	Porta *porta1 = gerar_NOT(&sinalA);
 	rede_portas[0] = porta1;
 
@@ -114,7 +145,11 @@ int main()
 	Porta *porta3 = gerar_AND(3, &sinalA, porta1, porta2);
 	rede_portas[2] = porta3;
 
+	Porta *porta4 = gerar_OR(2, &sinalA, porta2);
+	rede_portas[3] = porta4;
+	
 	update(rede_portas);
+//
 
 	printf("Entrada A \nSaida: %d\n\n", sinalA.saida);
 	printf("Porta NOT \nEntrada: %d \nSaida: %d\n\n", *porta1->entrada[0], porta1->saida);
@@ -127,7 +162,20 @@ int main()
 		printf(", %d", *porta3->entrada[i]);
 	}
 
-	printf("\nSaida: %d\n", porta3->saida);
+	printf("\nSaida: %d\n\n", porta4->saida);
+	
+	printf("Porta OR \nEntrada: %d", *porta4->entrada[0]);
+
+	for (size_t i = 1; i < porta4->quant_entradas; i++)
+	{
+		printf(", %d", *porta4->entrada[i]);
+	}
+
+	printf("\nSaida: %d\n", porta4->saida);
+
+
+
+
 
 	return 0;
 }
